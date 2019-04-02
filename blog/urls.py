a0fe9e334b54ea74d6e12e7a46b451f51cd46877
Yasser <yasserkotrsi@gmail.com>
2019-04-02 14:14:15 +0100

    Adding remove and edit functionalities

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.posts_list, name='posts_list'),
+	path('', views.posts_list, name='post_list'),
 	path('addPost', views.add_post, name='add_post'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
-	path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
+	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('drafts/', views.post_draft_list, name='post_draft_list'),
+	path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+	path('post/<int:pk>/remove', views.post_remove, name='post_remove')
 ]
